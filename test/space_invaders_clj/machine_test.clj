452003fbd36061f8c15159c4f82f808fe3cf4e44
Matt Grimm <matt.t.grimm@gmail.com>
2020-08-09 19:48:59 -0600

    Fix test

--- test/space_invaders_clj/machine_test.clj
+++ test/space_invaders_clj/machine_test.clj
@@ -5,20 +5,20 @@
 
 (deftest handle-in-port3-test
   (let [m {:mach/sh-hi 0xff :mach/sh-lo 0}]
-    (= 2r11111111 (:cpu/a (machine/handle-in (assoc m :mach/sh-off 0) 3)))
-    (= 2r11111100 (:cpu/a (machine/handle-in (assoc m :mach/sh-off 2) 3)))
-    (= 2r00000000 (:cpu/a (machine/handle-in (assoc m :mach/sh-off 7) 3)))))
+    (= 2r11111111 (:cpu/a (machine/handle-in (atom (assoc m :mach/sh-off 0)) 3)))
+    (= 2r11111100 (:cpu/a (machine/handle-in (atom (assoc m :mach/sh-off 2)) 3)))
+    (= 2r00000000 (:cpu/a (machine/handle-in (atom (assoc m :mach/sh-off 7)) 3)))))
 
 (deftest handle-out-port2-test
   (let [{:keys [:mach/sh-off] :as new-m}
-        (machine/handle-out {:cpu/a 2r110} 2)]
+        (machine/handle-out (atom {:cpu/a 2r110}) 2)]
     (is (= 2r110 sh-off))))
 
 (deftest handle-out-port4-test
   (reduce
     (fn [m [x [exp-hi exp-lo]]]
       (let [{:keys [:mach/sh-lo :mach/sh-hi :cpu/a] :as new-m}
-            (machine/handle-out (assoc m :cpu/a x) 4)]
+            (machine/handle-out (atom (assoc m :cpu/a x)) 4)]
         (is (= exp-lo sh-lo))
         (is (= exp-hi sh-hi))
         (is (= x a))
